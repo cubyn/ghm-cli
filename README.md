aab6b648f1a4772b341de6d8596e7d8482c8fa95
Mathieu LEMAIRE <maz.spam@gmail.com>
2017-03-02 13:51:13 +0100

    Add list command

--- README.md
+++ README.md
@@ -19,7 +19,8 @@ $ ghm --help
 ghm <cmd> [args]
 
 Commands:
-  login               Create and store a Github access token
+  login               Login to Github (no password ever stored)
+  list                List milestones in all your repos
   close [milestone]   Close milestone
   open [milestone]    Re-open milestone
   update [milestone]  Update milestone
@@ -28,6 +29,7 @@ Options:
   --help  Show help   [boolean]
 
 Examples:
+  list --state all                              Will list all milestones accross your repos
   close "Milestone #1"                          Will close all milestones named "Milestone #1"
   update "Milestone #1" --title milestone-1     Will rename all milestones named "Milestone #1" to "milestone-1"
 ```
--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const yargs = require('yargs');
 const update = require('./lib/commands/update');
 const changeStatus = require('./lib/commands/change-status');
 const login = require('./lib/commands/login');
+const list = require('./lib/commands/list');
 
 process.on('unhandledRejection', (error, promise) => {
     console.log(error.stack);
 });
 
-yargs
+const argv = yargs
     .usage('$0 <cmd> [args]')
-    .command('login', 'Create and store a Github access token', {}, login)
+    .command('login', 'Login to Github (no password ever stored)', {}, login)
+    .command('list', 'List milestones in all your repositories', {
+        state: {
+            alias: 's',
+            type: 'state',
+            choices: ['all', 'open', 'closed'],
+            describe: 'force update on closed milestones as well'
+        }
+    }, list)
     .command('close [milestone]', 'Close milestone', {}, changeStatus('closed'))
     .command('open [milestone]', 'Re-open milestone', {}, changeStatus('open'))
     .command('update [milestone]', 'Update milestone', {
@@ -34,6 +43,9 @@ yargs
     }, update)
     .example('close "Milestone #1"', 'Will close all milestones named "Milestone #1"')
     .example('update "Milestone #1" --title milestone-1', 'Will rename all milestones named "Milestone #1" to "milestone-1"')
-    .help()
     .argv;
 
+if (!argv._[0]) {
+    yargs.showHelp();
+    process.exit(1);
+}
